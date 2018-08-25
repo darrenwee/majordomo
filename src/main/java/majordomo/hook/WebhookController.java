047ab7b47021c79c3e1cf312208dc2777e104c8c
Cara Leong <heycraa@gmail.com>
2018-08-25 16:23:31 +0800

    Actually run the controller

--- src/main/java/majordomo/hook/WebhookController.java
+++ src/main/java/majordomo/hook/WebhookController.java
@@ -1,5 +1,6 @@
 package majordomo.hook;
 
+import majordomo.controller.Controller;
 import majordomo.github.event.GithubIssueCommentEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebhookController {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    private Controller controller;
+
+    public WebhookController(Controller controller) {
+        this.controller = controller;
+    }
+
     @PostMapping(value = "/pr", consumes = "application/json")
     public GithubIssueCommentEvent receive(@RequestBody GithubIssueCommentEvent payload) {
         logger.info("Received payload on /pr: {}", payload.toString());
+        controller.run(payload);
         return payload;
     }
 }
